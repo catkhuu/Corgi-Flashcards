364cd472372dc713354586419ae48d71edff312d
Catherine Khuu <sunshine.dumbo@gmail.com>
2016-10-07 16:50:20 -0400

    Create sessions helper.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
   has_many :guesses, through: :rounds
   has_many :played_decks, through: :rounds, source: :deck
 
-  validates :username, :email, presence: true
-  validates_uniqueness_of :email
+  validates :username, :email, presence: true, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
